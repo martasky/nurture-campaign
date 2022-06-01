5dbe7eff11c36bd7c4c0b4e55413291070f1d0e0
Marta Skrzypczak <mart51w7@stud.kea.dk>
2022-06-01 14:08:24 +0200

    fix words changing in loop

--- components/landingPage/getAdjectives.js
+++ components/landingPage/getAdjectives.js
@@ -1,25 +1,32 @@
 import React from "react";
 
 const Loader = (props) => {
-  const {adjectives} = props;
-    const [adjectivesIndex, setAdjectiveIndex] = React.useState(0);
-    const colors = ["#6C06D1","#ffad09", "#ff72b9", "#39b4f9"];
-    
+  const { adjectives } = props;
+  const [adjectivesIndex, setAdjectiveIndex] = React.useState(0);
 
-    React.useEffect(() => {
-      let timeout;
-      if (adjectivesIndex < adjectives.length -1) {
-        timeout = setTimeout(() => setAdjectiveIndex(adjectivesIndex +1), 2500);
-      }
-      return () => {
-        clearTimeout(timeout)
-      };
-    }, [adjectives, adjectivesIndex]);
-    
-    return <div style={{color:colors[adjectivesIndex]}}>{adjectives[adjectivesIndex]}</div>
-  }
+  const colors = ["#6C06D1", "#ffad09", "#ff72b9", "#39b4f9"];
 
-  export default Loader
+  React.useEffect(() => {
+    let timeout;
 
+    timeout = setTimeout(
+      () =>
+        setAdjectiveIndex((prevIndex) => {
+          return prevIndex + 1 < adjectives.length ? prevIndex + 1 : 0;
+        }),
+      2500
+    );
 
-  
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [adjectives, adjectivesIndex]);
+
+  return (
+    <div style={{ color: colors[adjectivesIndex] }}>
+      {adjectives[adjectivesIndex]}
+    </div>
+  );
+};
+
+export default Loader;
